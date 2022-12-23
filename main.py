a74afe302970bba723be0abecaa93d72d28535cc
AhmedGebril <40351175+AhmedGebril@users.noreply.github.com>
2022-12-23 03:54:59 +0200

    done a few tweaks

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import time
-
 from scraping import Scrabing
 import twilio
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import smtplib
 from unidecode import unidecode
 from tkinter import *
 
-def start():
-    BUY_PRICE=price.get()
-    now_time =datetime.now().strftime("%I:%M %p")
-    print(now_time)
-    if now_time == "08:00 AM":
-        searching = Scrabing()
+
+def radio_choice():
+    state = radio_state.get()
+    if state == 'Everyday':
+        day = (24 * 60 * 60)
+        start(day)
+    elif state == 'Everyweek':
+        week = (6 * 24 * 60 * 60)
+        start(week)
+
+def start(waiting_time):
+    BUY_PRICE = price.get()
+    now_time = datetime.now().strftime("%I:%M %p")
+    print(BUY_PRICE)
+    if now_time == "03:53 AM":
+        searching = Scrabing(url=url_Entry.get())
         price_without_tag = searching.Get_Price()
+        print(price_without_tag)
         product_title = searching.Get_Title().strip()
         print(unidecode(product_title))
         if price_without_tag <= float(BUY_PRICE):
@@ -21,10 +31,17 @@ def start():
             print("message sent.")
             with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
                 connection.starttls()
-                connection.login(user="ahmedgebril1889@gmail.com", password="gatcrnogtbtxbkdh")
+                # Enter your Private Email and password here.
+                connection.login(user=, password=)
                 connection.sendmail(from_addr="ahmedgebril1889@gmail.com",
                                     to_addrs="ahmedgebril1889@gmail.com",
                                     msg=f"Subject:\n\n{unidecode(product_title)} is now {BUY_PRICE} or less.")
+        else:
+            print('still not the right price')
+    else:
+        print('still waiting for the right time')
+        time.sleep(waiting_time)
+
 
 
 window = Tk()
@@ -32,29 +49,28 @@ window.title("price tracker")
 window.geometry("400x250")
 
 label1 = Label(text="Welcome to Amazon Tracker App.")
-label1.grid(row=0,column=1,columnspan=3)
+label1.grid(row=0, column=1, columnspan=3)
 
-url_label=Label(text="Enter Your url")
-url_label.grid(row=4,column=0)
+url_label = Label(text="Enter Your url")
+url_label.grid(row=4, column=0)
 
-url_Entry=Entry(width=30)
-url_Entry.grid(row=4,column=1)
+url_Entry = Entry(width=30)
+url_Entry.grid(row=4, column=1)
 
-price_label=Label(text="price you want to track")
-price_label.grid(row=5,column=0)
+price_label = Label(text="price you want to track")
+price_label.grid(row=5, column=0)
 
-price=IntVar()
-price_Entry=Entry(width=30,textvariable=price)
-price_Entry.grid(row=5,column=1)
+price = IntVar()
+price_Entry = Entry(width=30, textvariable=price)
+price_Entry.grid(row=5, column=1)
 
-track_btn=Button(text="Track Price",command=start,highlightthickness=1)
-track_btn.grid(row=6,column=1)
+track_btn = Button(text="Track Price", command=radio_choice, highlightthickness=1)
+track_btn.grid(row=6, column=1)
 
-radio_state=StringVar()
-radio1=Radiobutton(variable=radio_state,text="Everyday",value='9:00 PM')
-radio2=Radiobutton(variable=radio_state,text="Everyweek",value='10:00 PM')
-radio1.grid(row=7,column=0)
-radio2.grid(row=7,column=1)
-
+radio_state = StringVar()
+radio1 = Radiobutton(variable=radio_state, text="Everyday", value='Everyday')
+radio2 = Radiobutton(variable=radio_state, text="Everyweek", value='Everyweek')
+radio1.grid(row=7, column=0)
+radio2.grid(row=7, column=1)
 
 window.mainloop()
--- scraping.py
+++ scraping.py
@@ -5,8 +5,8 @@ import lxml
 
 
 class Scrabing:
-    def __init__(self):
-        self.url = "https://www.amazon.com/dp/B075L7QHCH/ref=sbl_dpx_kitchen-electric-cookware_B08GC6PL3D_0"
+    def __init__(self,url):
+        self.url = url
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53",
             "Accept-Language": "en-US,en;q=0.9"}
@@ -15,7 +15,8 @@ class Scrabing:
 
     def Get_Price(self):
         price_number = self.soup.find(name="span", class_="a-offscreen").getText()
-        price_without_tag = float(price_number.split("$")[1])
+        print(price_number)
+        price_without_tag = float(price_number.split("EGP")[1])
         return price_without_tag
 
     def Get_Title(self):
